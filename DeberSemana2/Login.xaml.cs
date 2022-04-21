67bc65215bee07fe7566f66fc36d07eab5566d3d
cruiz777 <cristian_ruiz_777@hotmail.com>
2022-04-21 13:25:38 -0500

    Agregar archivos de proyecto.

---
+++ DeberSemana2/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DeberSemana2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void Ingresar_Clicked(object sender, EventArgs e)
+        {
+
+            string usuario = txtusuario.Text;
+            string clave=txtclave.Text;
+            if (usuario == "estudiante2022" && clave== "uisrael2022")
+            {
+                await Navigation.PushAsync(new MainPage(usuario));
+            }
+            else
+            {
+                _ = DisplayAlert("Advertencia", "Usuario o Clave Incorrecta", "Ok");
+            }
+
+            
+        }
+    }
+}
---
+++ DeberSemana2/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DeberSemana2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(string user)
+        {
+            
+            InitializeComponent();
+            txtnombre.Text = user;
+        }
+
+        private void suma_Clicked(object sender, EventArgs e)
+        {
+            double nt1 =Convert.ToDouble( ns1.Text);
+            double ntx1 = Convert.ToDouble(ex1.Text);
+            double pp1 = (nt1 * 0.3) + (ntx1 * 0.2);
+            double nt2 = Convert.ToDouble(ns2.Text);
+            double ntx2 = Convert.ToDouble(ex2.Text);
+            double pp2 = (nt2 * 0.3) + (ntx2 * 0.2);
+            double suma =pp1 +pp2;
+            p1.Text = Convert.ToString(pp1);
+            p2.Text = Convert.ToString(pp2);
+            nfinal.Text = Convert.ToString(suma);
+            if (suma>=7)
+            {
+                DisplayAlert("Resultado", "Aprobado", "OK");
+            }
+            if (suma <5)
+            {
+                DisplayAlert("Resultado", "Reprobado", "OK");
+            }
+            if (suma >= 5 && suma<=6.9)
+            {
+                DisplayAlert("Resultado", "Complementario", "OK");
+            }
+
+
+        }
+    }
+}
